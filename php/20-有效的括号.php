ca6dbd54c930e434cb772a0e5c3eae70de8ab434
huanglei <huanglei@quanjingshuju.com>
2021-03-09 09:57:37 +0800

    修改结构.

--- php/20-有效的括号.php
+++ php/20-有效的括号.php
@@ -8,19 +8,16 @@ class Solution {
     function isValid($s) {
         //后进先出: 遍历整个字符串s 如果是左括号将其压入栈中 右括号将栈中元素取出对比
         $validChar = ['(', '{', '['];
-		$matchDictionary = [']' => '[', '}' => '{', ')' => '('];
-		
-		//创建一个数组当作栈 array_push作为压栈操作 array_pop作为出栈操作
-		$stack = [];
-		
-        //匹配至少应满足2个元素
+        $matchDictionary = [']' => '[', '}' => '{', ')' => '('];
+            
+        //创建一个数组当作栈 array_push作为压栈操作 array_pop作为出栈操作
+        $stack = [];
+        
+        //字符串长度
         $length = strlen($s);
-        if ($length < 2) {
-            return false;
-        }
-
+        
         //遍历字符串
-        for($i = 0; $i < $length; $i++)
+        for ($i = 0; $i < $length; $i++)
         {
             if(in_array($s[$i], $validChar)) {
                 //左括号压入栈中
@@ -32,8 +29,8 @@ class Solution {
                 }
             }
         }
-		
-		//到最后数组应该全部匹配完
-		return empty($stack) ? true : false;
+                
+        //到最后数组应该全部匹配完
+        return empty($stack) ? true : false;
     }
 }
