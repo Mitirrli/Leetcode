64e5bad46f84af3c2d6638a84028888bb29f4579
huanglei <huanglei@quanjingshuju.com>
2021-03-18 09:53:32 +0800

    最长公共前缀 递归解法.

--- php/14-最长公共前缀.php
+++ php/14-最长公共前缀.php
@@ -7,28 +7,19 @@ class Solution {
      * @return String
      */
     function longestCommonPrefix($strs) {
-        $len = array_map('strlen', $strs);
-        $min = min($len); //计算长度为最短的长度
-    
+        $min = min(array_map('strlen', $strs)); //计算长度为最短的长度
         $prefix = substr($strs[0], 0, $min);
+
         array_shift($strs); //去掉第一个元素
-    
         foreach ($strs as $value) {
             $prefix = $this->checkInclude($value, $prefix);
         }
-    
-    
+
         return $prefix;
     }
-    
+
     function checkInclude($value, $str)
     {
-        if (strpos($value, $str) === 0) {
-            $prefix = $str;
-    
-            return $prefix;
-        } else {
-            return $this->checkInclude($value, substr($str, 0, -1));
-        }
+        return strpos($value, $str) === 0 ? $str : $this->checkInclude($value, substr($str, 0, -1));
     }
-    }
+}
